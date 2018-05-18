9ba099c96f20deacf869f45aaa8d3016ae461a18
gokhandemirhan <demirhangokhan@gmail.com>
2018-05-19 01:26:47 +0200

    Variable configs

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+/*
+ * Heroku has a NODE_ENV variable
+ */
+if(process.env.NODE_ENV === 'production'){
+	// in prod, return prod keys
+	module.exports = require("./prod");
+}else{
+	// return dev keys
+	module.exports = require("./dev");
+}
